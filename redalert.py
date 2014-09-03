cbcd19f91be809287e59775868713adbc74df5bb
Dotan Cohen <dotan@webtov.com>
2014-09-03 13:24:55 +0300

    Use tabs

--- redalert.py
+++ redalert.py
@@ -17,59 +17,59 @@ from urllib.error import URLError
 
 def main():
 
-    url = 'http://www.oref.org.il/WarningMessages/alerts.json'
-    last_data = ''
-    affected_areas = []
-    interested_areas = [287, 288, 289, 291, 292, 297]
+	url = 'http://www.oref.org.il/WarningMessages/alerts.json'
+	last_data = ''
+	affected_areas = []
+	interested_areas = [287, 288, 289, 291, 292, 297]
 
-    while True:
-        current_time = datetime.datetime.now().strftime('%H:%M:%S') + ' '
-        try:
-            response = urllib.request.urlopen(url, timeout=1).read()
+	while True:
+		current_time = datetime.datetime.now().strftime('%H:%M:%S') + ' '
+		try:
+			response = urllib.request.urlopen(url, timeout=1).read()
 
-            # 65279 is the zero-width no-break space, also used
-            # as and more commonly known as the BOM!
-            response_decoded = re.sub(chr(65279), '', response.decode('utf-16le'))
+			# 65279 is the zero-width no-break space, also used
+			# as and more commonly known as the BOM!
+			response_decoded = re.sub(chr(65279), '', response.decode('utf-16le'))
 
-            predata = json.loads(response_decoded)
-            data = predata['data']
+			predata = json.loads(response_decoded)
+			data = predata['data']
 
-            if len(data)==0:
-                last_data = ''
-                continue
+			if len(data)==0:
+				last_data = ''
+				continue
 
-            affected_areas[:] = []
-            for d in data:
-                affected_area = d.split()[-1:][0]
-                if affected_area.isdigit():
-                    affected_areas.append(int(affected_area))
-                else:
-                    print('No number for this area: ' + str(d))
-                    continue
+			affected_areas[:] = []
+			for d in data:
+				affected_area = d.split()[-1:][0]
+				if affected_area.isdigit():
+					affected_areas.append(int(affected_area))
+				else:
+					print('No number for this area: ' + str(d))
+					continue
 
-            for ia in interested_areas:
-                if ia in affected_areas:
-                    os.system('beep')
-                    if data!=last_data:
-                        last_data = data
-                        print(current_time + str(data))
-                    continue
+			for ia in interested_areas:
+				if ia in affected_areas:
+					os.system('beep')
+					if data!=last_data:
+						last_data = data
+						print(current_time + str(data))
+					continue
 
-            if data!=last_data:
-                last_data = data
-                #print( current_time + 'Different area: ' + str(data))
+			if data!=last_data:
+				last_data = data
+				#print( current_time + 'Different area: ' + str(data))
 
-        except (URLError, timeout) as e:
-            #print( current_time + 'Timeout, probably not a big deal.')
-            pass
-        except Exception as e:
-            print( current_time + 'Exception: ' + str(e))
+		except (URLError, timeout) as e:
+			#print( current_time + 'Timeout, probably not a big deal.')
+			pass
+		except Exception as e:
+			print( current_time + 'Exception: ' + str(e))
 
-        time.sleep(1)
+		time.sleep(1)
 
-    return True
+	return True
 
 
 
 if __name__ == '__main__':
-    main()
+	main()
